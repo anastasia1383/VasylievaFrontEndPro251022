395b7f8ba1771f507d03e94cb72a941994ad7a28
Anastasiia Vasylieva <anastasia.vasylieva97@gmail.com>
2022-10-25 22:26:50 +0300

    hw_1 fix

--- hw1/index.js
+++ hw1/index.js
@@ -1,2 +1,4 @@
-const userName= prompt('What is your name?');
-alert('Hello '+userName+' ! How are you?');
+window.addEventListener('DOMContentLoaded', (event) => {
+    const userName= prompt('What is your name?');
+    alert('Hello '+userName+' ! How are you?');
+});
