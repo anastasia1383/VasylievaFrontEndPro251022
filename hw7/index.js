c78f5f44c3ad0a5b5b64ae40a9a8900316b3c0a9
Anastasiia Vasylieva <anastasia.vasylieva97@gmail.com>
2022-10-28 23:48:18 +0300

    hw_7 done

---
+++ hw7/index.js
@@ -0,0 +1,7 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    const firstNumber = Number(prompt("Enter 1st number"));
+    const secondNumber = Number(prompt("Enter 2nd number"));
+    const thirdNumber = Number(prompt("Enter 3rd number"));
+
+    alert((firstNumber + secondNumber + thirdNumber)/3);
+});
