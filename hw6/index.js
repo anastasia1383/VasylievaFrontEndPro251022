cf27be87539c50362c52d302e7752835e5ba5f36
Anastasiia Vasylieva <anastasia.vasylieva97@gmail.com>
2022-10-29 00:18:19 +0300

    hw_6 done

---
+++ hw6/index.js
@@ -0,0 +1,22 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    const operator = prompt("Please choose an operation (add, sub, mult, div)")
+    const firstNumber = Number(prompt("Enter 1st number"));
+    const secondNumber = Number(prompt("Enter 2nd number"));
+
+    let result = "Wrong operation selected!";
+
+    if (operator === "add") {
+        result = `${firstNumber}+${secondNumber} = ${firstNumber + secondNumber}`;
+    }
+    if (operator === "sub") {
+        result = `${firstNumber}-${secondNumber} = ${firstNumber - secondNumber}`;
+    }
+    if (operator === "mult") {
+        result = `${firstNumber}*${secondNumber} = ${firstNumber * secondNumber}`;
+    }
+    if (operator === "div") {
+        result = `${firstNumber}/${secondNumber} = ${firstNumber / secondNumber}`;
+    }
+
+    alert(result);
+});
