d3df6724fa1cabe7b722db3996d2ac6079669945
Anastasiia Vasylieva <anastasia.vasylieva97@gmail.com>
2022-11-02 21:04:17 +0200

    hw8 done

---
+++ hw_8/index.js
@@ -0,0 +1,48 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    const userBYear = prompt('Введіть Ваш рік народження.', '');  
+    const userCity = prompt('В якому місті Ви проживаєте?', '');
+    const userSport = prompt('Введіть Ваш улюблений вид спорту.', '');
+
+    const today = new Date().getFullYear();
+    const userAge = today - +userBYear;
+
+    let result = userBYear ? `Ваш вік: ${userAge} р.\n` : 'Шкода, що Ви не захотіли ввести свій рік народження :(\n';
+
+    const kyiv = 'київ';
+    const washington = 'вашингтон';
+    const london = 'лондон';
+
+    const citiesCountries = {
+        [kyiv]: 'України',
+        [washington]: 'Америки',
+        [london]: 'Великобританії',
+    };
+
+    const chess = 'шахи';
+    const whiffWhaff = 'пінг-понг';
+    const curling = 'керлінг';
+
+    const sportsChamps = {
+        [chess]: 'Магнус Карлсен',
+        [whiffWhaff]: 'Фань Чженьдун',
+        [curling]: 'Сільвана Тірінзоні',
+    };
+
+    if (userCity && citiesCountries[userCity.toLowerCase()]) {
+        result += `Ви живете у столиці ${citiesCountries[userCity.toLowerCase()]}.\n`;
+    } else if (userCity) {
+        result += `Ви живете у місті ${userCity}.\n`;
+    } else {
+        result += 'Шкода, що Ви не захотіли ввести своє місто :(\n';
+    }
+
+    if (userSport && sportsChamps[userSport.toLowerCase()]) {
+        result += `Круто! Хочеш стати як ${sportsChamps[userSport.toLowerCase()]}?`;
+    } else if (userSport) {
+        result += `Твій улюблений спорт ${userSport}.`;
+    } else {
+        result += 'Шкода, що Ви не захотіли ввести свій улюблений спорт :(';
+    }
+
+    alert(result);
+});
